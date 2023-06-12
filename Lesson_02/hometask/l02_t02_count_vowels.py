7959d50e60cc8ceafdc8f2ad7d6efdb9a7f6853f
Anton Volkov <qa.volkov@gmail.com>
2023-06-12 11:14:54 +0200

    HW:2_Volkov

--- Lesson_02/hometask/l02_t02_count_vowels.py
+++ Lesson_02/hometask/l02_t02_count_vowels.py
@@ -1,6 +1,8 @@
+# Homework: Lesson 2 [Volkov Anton]
 # The English language has five vowels: A, E, I, O, and U
-# Please count each vowel occurence in text bellow ( sum of lower and capital cases)
-# and write output as table smth like this
+# Please count each vowel occurrence in the text below
+# (sum of lowercase and capital cases)
+# and write output as a table, something like this:
 # -----------------
 # | vowel | count |
 # -----------------
@@ -9,17 +11,12 @@
 #   ...
 # -----------------
 
-# then modify text where each vowel replaced with
-# A->À;  a->à ; E-> É ; e->é; I->Í , i->í ; O->Ó ; o->ó; U->Ú; u->ú
-# ex. "Í wàndéréd lónély...."   and print it
-
-
 poem_text = """I wandered lonely as a cloud
 That floats on high o'er vales and hills,
 When all at once I saw a crowd,
 A host, of golden daffodils;
 Beside the lake, beneath the trees,
-Fluttering and dancing in the breeze. 
+Fluttering and dancing in the breeze.
 
 Continuous as the stars that shine
 And twinkle on the Milky Way,
@@ -27,3 +24,36 @@ They stretched in never-ending line
 Along the margin of a bay:
 Ten thousand saw I at a glance,
 Tossing their heads in sprightly dance."""
+
+# 1. Count each vowel occurrence in the text
+# (sum of lowercase and capital cases)
+# and write output as a table
+sep_num = 17
+print('-' * sep_num)
+print(f'|{"vowel":^7}|{"count":^7}|')
+print('-' * sep_num)
+print(
+    f'|{"a":^7}|{poem_text.lower().count("a"):^7}|' +
+    f'\n|{"e":^7}|{poem_text.lower().count("e"):^7}|' +
+    f'\n|{"i":^7}|{poem_text.lower().count("i"):^7}|' +
+    f'\n|{"o":^7}|{poem_text.lower().count("o"):^7}|' +
+    f'\n|{"u":^7}|{poem_text.lower().count("u"):^7}|',
+)
+print('-' * sep_num)
+
+# 2. Modify the text where each vowel is replaced with
+# A->À;  a->à ; E-> É ; e->é; I->Í , i->í ; O->Ó ; o->ó; U->Ú; u->ú
+# and print it
+poem_text_replaced = (
+    poem_text.replace('A', 'À')
+    .replace('a', 'à')
+    .replace('E', 'É')
+    .replace('e', 'é')
+    .replace('I', 'Í')
+    .replace('i', 'í')
+    .replace('O', 'Ó')
+    .replace('o', 'ó')
+    .replace('U', 'Ú')
+    .replace('u', 'ú')
+)
+print(poem_text_replaced)
